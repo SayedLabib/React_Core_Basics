98c87362b492e6cc7cf9e7d012bcef93e842c57d
SayedLabib <alfuad1007@gmail.com>
2026-03-12 03:45:09 +0600

    React mulptuple ways of event handler

--- react-core-concepts-2/src/App.jsx
+++ react-core-concepts-2/src/App.jsx
@@ -3,31 +3,40 @@ import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
-function App() {
-  const [count, setCount] = useState(0)
 
+function App() {
+
+ function handleClick() {
+  alert('Button clicked!')
+}
+
+const handleClick3 = (num) => {
+  alert(num + 2)
+}
   return (
     <>
-      <div>
-        <a href="https://vite.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.jsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <h3 style={{ 
+        color: 'LightSeaGreen',
+       }}>React Core Concepts Part 2</h3>
+
+       <button onClick={handleClick} style={{
+        background:'Lightblue',
+        border:'none',
+        borderCollapse:'collapse',
+        cursor:'pointer',
+        borderRadius:'8px',
+
+       }}>Click Here</button>
+
+       <button onClick={()=> alert('Button 2 clicked!')} style={{
+        background:'Lightcoral',
+        border:'none',
+        borderCollapse:'collapse',
+        cursor:'pointer',
+        borderRadius:'8px',
+       }}>Click 2</button>
+      
+      <button onClick={() => handleClick3(5)} >Click 3</button>
     </>
   )
 }
