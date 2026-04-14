6f1b044c5167867e06b2a31e14c23ecf2f86f5d5
SayedLabib <alfuad1007@gmail.com>
2026-04-14 22:27:31 +0600

    Knowledge Cafe project setup and Data load has been completed

--- knowledge-cafe/src/App.jsx
+++ knowledge-cafe/src/App.jsx
@@ -1,16 +1,17 @@
 
 import './App.css'
+import Header from './components/Header/Header'
+import Blogs from './components/Blogs/Blogs'
+
 
 function App() {
 
 
   return (
     <>
-
-          <h1 className='text-3xl font-bold'>
-           knowledge Cafe
-          </h1>
-
+       <Header></Header>
+      <Blogs></Blogs>
+  
     </>
   )
 }
---
+++ knowledge-cafe/src/components/Header/Header.jsx
@@ -0,0 +1,17 @@
+
+
+const Header = () => {
+    
+    return (
+        <div>
+            
+          <h1 className='text-3xl font-bold mt-10'>
+           knowledge Cafe
+          </h1>
+
+
+        </div>
+    );
+};
+
+export default Header;
